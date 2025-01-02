e4734dbce090cdafd0deda09caa78b9cf114196a
shyam1998 <shyamsundarsahoo1998@gmail.com>
2025-01-02 08:12:25 +0530

    data type, function, array, union, alias declaration in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,25 @@
 "use strict";
-const a = 257;
-console.log("a", a);
+// Data types in typescript
+//String
+let a = "Hello Shyam"; //1st way to specify type of data type.
+let b = "Hello Dipu"; //2nd way to specify type of data type.
+//Number
+let c = 1234;
+let d = 123;
+//Boolean
+let e = true;
+let f = false;
+//String or Number type
+let g = 123;
+let h = "Hello Shyam";
+//function in typescript
+const func = (n, m) => {
+    return n * m;
+};
+//array
+const arr = [1, 2, 3, 4];
+const arr1 = ["shyam", "dipu", "sandi"];
+const arr2 = ["shyam", 2, 3];
+const arr3 = [1, 2, 3, 4];
+const arr4 = ["shyam", "dipu", "sandi"];
+const arr5 = ["shyam", 2, 3];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-const a = 257;
+// Data types in typescript
 
-console.log("a", a)
+//String
+let a: string = "Hello Shyam"; //1st way to specify type of data type.
+
+let b = <string>"Hello Dipu"; //2nd way to specify type of data type.
+
+//Number
+let c: number = 1234;
+
+let d = <number>123;
+
+//Boolean
+let e: boolean = true;
+
+let f = <boolean>false;
+
+//String or Number type
+let g: boolean | number = 123;
+
+let h: string | number = "Hello Shyam";
+
+// Alias type
+type UserName = (n: number, m: number) => number;
+
+//function in typescript
+const func: UserName = (n, m) => {
+  return n * m;
+};
+
+//array
+const arr: number[] = [1, 2, 3, 4];
+
+const arr1: string[] = ["shyam", "dipu", "sandi"];
+
+const arr2: (string | number)[] = ["shyam", 2, 3];
+
+const arr3: Array<number> = [1, 2, 3, 4];
+
+const arr4: Array<string> = ["shyam", "dipu", "sandi"];
+
+const arr5: Array<string | number> = ["shyam", 2, 3];
